8d440f5d95ff624f788296d2c2619e94d9113aa2
mathyc0de <mateushackrs7@gmail.com>
2024-11-18 01:22:07 -0300

    🚧 in progress: UI

--- main.js
+++ main.js
@@ -1,31 +1,57 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu, ipcMain } = require('electron');
 const path = require('path');
 
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
+
+
+let mainWindow;
+let tray;
+
 function createWindow() {
-  const mainWindow = new BrowserWindow({
+    if (tray != null) {
+        tray.destroy();
+    }
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    fullscreen: true,
+    frame: true,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, './src/preload.js'),
       contextIsolation: false,
       enableRemoteModule: false,
       nodeIntegration: true
     }
   });
-
   mainWindow.loadFile('index.html');
+  mainWindow.webContents.openDevTools()
+  mainWindow.on('resize', () => {})
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    
   }
 });
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
+  }
+});
+
+ipcMain.on('exit-fullscreen', () => {
+  if (mainWindow.isVisible()) {
+    mainWindow.hide()
+    tray = new Tray("./assets/icon.png")
+    tray.on('click', () => createWindow())
+      const contextMenu = Menu.buildFromTemplate([
+          { label: "Sair", type: "normal", click: () => app.quit()},
+      ]);
+  tray.setContextMenu(contextMenu)
   }
 });
---
+++ src/background_shader.js
@@ -0,0 +1,51 @@
+// background_shader.js
+
+const THREE = require('three');
+
+
+
+class Background {
+    constructor() {
+        this.sizes = {
+            width: window.innerWidth,
+            height: window.innerHeight
+          }
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, this.sizes.width / this.sizes.height, 0.1, 1000);
+        const canvas = this.canvas = document.querySelector('.webgl')
+        this.renderer = new THREE.WebGLRenderer({canvas});
+        this.render()
+        this.animate = this.animate.bind(this); // Bind the animate method to the correct context
+    }
+
+
+    render() {
+        this.renderer.setSize(this.sizes.width, this.sizes.height);
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        this.cube = new THREE.Mesh(geometry, material);
+        this.scene.add(this.cube);
+        this.camera.position.z = 5;
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    animate() {
+        requestAnimationFrame(this.animate);
+        this.cube.rotation.x += 0.01;
+        this.cube.rotation.y += 0.01;
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    resize() {
+        console.log(innerWidth)
+        this.sizes.width = innerWidth
+        this.sizes.height = innerHeight
+        this.camera.aspect = this.sizes.width / this.sizes.height
+        this.camera.updateProjectionMatrix()
+        this.renderer.setSize(this.sizes.width, this.sizes.height)
+    }
+
+}
+
+
+module.exports = { Background };
