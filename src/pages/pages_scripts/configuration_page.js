455b31501d572a07c89950b22ae608d600607725
mathyc0de <mateushackrs7@gmail.com>
2024-11-30 16:14:57 -0300

    ✨ feat: file picker

--- src/pages/pages_scripts/configuration_page.js
+++ src/pages/pages_scripts/configuration_page.js
@@ -1,9 +1,26 @@
-const form = document.querySelector(".config-form")
+const fs = require('fs')
+const { join } = require('path')
 
-console.log(form);
 
-document.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const input = form.querySelector("input");
-    console.log(input.value);
-})
+const path = 'C:/'
+const parent = document.querySelector('.folders')
+
+const updateFolders = function (path) {
+    parent.innerHTML = ''
+    const folders = fs.readdirSync(path)
+    folders.forEach((value, idx) => {
+        const folder = document.createElement('div')
+        folder.className = 'folder'
+        folder.id = idx
+        folder.textContent = value
+        folder.addEventListener('click', () => {
+            const folder = value + '/'
+            const newPath = join(path, folder)
+            console.log(newPath)
+            updateFolders(newPath)
+        })
+        parent.appendChild(folder)
+    })
+}
+
+updateFolders(path)
