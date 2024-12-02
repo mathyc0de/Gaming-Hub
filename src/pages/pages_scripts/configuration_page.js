001a9379917afeb146464a008004d6bee8093c52
mathyc0de <mateushackrs7@gmail.com>
2024-12-02 19:43:24 -0300

    ✨ feat: get Windows Drives

--- src/pages/pages_scripts/configuration_page.js
+++ src/pages/pages_scripts/configuration_page.js
@@ -1,26 +1,48 @@
 const fs = require('fs')
 const { join } = require('path')
 
+const getWindowsDrives = function () {
+    const drives = []
+    for (let idx = 65; idx < 91; idx++) {
+        const word = String.fromCharCode(idx)
+        if (fs.existsSync(`${word}:/`)) drives.push(`${word}:/`)
+    }
+    return drives
+}
+
+
+
 
 let path
+let drives
+
 switch (process.platform) {
     case 'linux':
         path = '/home/'
         break;
     case 'win32':
-        path = 'C:/'
+        drives = getWindowsDrives()
+        path = ''
         break;
 }
 const parent = document.querySelector('.folders')
 
 const updateFolders = function (path) {
+    let folders;
     parent.innerHTML = ''
-    const folders = fs.readdirSync(path)
+    if (path == '') {
+        folders = drives
+    }
+    else {
+        folders = fs.readdirSync(path)
+    }
     folders.forEach((value, idx) => {
         const folder = document.createElement('div')
         folder.className = 'folder'
         folder.id = idx
+
         folder.textContent = value
+        
         folder.addEventListener('click', () => {
             const folder = value + '/'
             const newPath = join(path, folder)
