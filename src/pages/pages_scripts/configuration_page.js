c2857cee056b8444cdff1629457e1163f18529dc
mathyc0de <mateushackrs7@gmail.com>
2024-12-12 21:44:25 -0300

    ✨ feat: configuration page

--- src/pages/pages_scripts/configuration_page.js
+++ src/pages/pages_scripts/configuration_page.js
@@ -1,56 +1,19 @@
-const fs = require('fs')
-const { join } = require('path')
-
-const getWindowsDrives = function () {
-    const drives = []
-    for (let idx = 65; idx < 91; idx++) {
-        const word = String.fromCharCode(idx)
-        if (fs.existsSync(`${word}:/`)) drives.push(`${word}:/`)
-    }
-    return drives
+function changeStmPath() {
+    document.querySelector('.effectiveArea').innerHTML = ''
 }
 
 
 
 
-let path
-let drives
 
-switch (process.platform) {
-    case 'linux':
-        path = '/home/'
-        break;
-    case 'win32':
-        drives = getWindowsDrives()
-        path = ''
-        break;
-}
-const parent = document.querySelector('.folders')
 
-const updateFolders = function (path) {
-    let folders;
-    parent.innerHTML = ''
-    if (path == '') {
-        folders = drives
-    }
-    else {
-        folders = fs.readdirSync(path)
-    }
-    folders.forEach((value, idx) => {
-        const folder = document.createElement('div')
-        folder.className = 'folder'
-        folder.id = idx
 
-        folder.textContent = value
-        
-        folder.addEventListener('click', () => {
-            const folder = value + '/'
-            const newPath = join(path, folder)
-            console.log(newPath)
-            updateFolders(newPath)
-        })
-        parent.appendChild(folder)
-    })
+const actions = {
+    stmpath: changeStmPath,
+    color: console.log
 }
 
-updateFolders(path)
+const configs = document.querySelectorAll('.config')
+configs.forEach((element) => element.addEventListener('click', (ev) => {
+    actions[element.id].call(this)
+}))
