eee607e281ada607e553840805fce049e6ad7198
mathyc0de <mateushackrs7@gmail.com>
2024-12-13 00:21:23 -0300

    ✨ feat: add gamepad support to configuration page

--- src/pages/pages_scripts/configuration_page.js
+++ src/pages/pages_scripts/configuration_page.js
@@ -1,3 +1,19 @@
+const { scrollElements } = require('../scripts/shared/element_selector')
+const { buttons,  Actions } = require('../scripts/shared/gamepad')
+const { gamepadController } = require('../scripts/services/game_control')
+
+function gamepadLoop() {
+    if (gamepadController.updateLoop()) { 
+        const gamepad = gamepadController.getGamepad(1)
+        gamepad.onPressed(buttons.A, Actions.accept)
+        gamepad.onPressed(buttons.GUIDE, Actions.goHome)
+        gamepad.onPressed(buttons.B, Actions.goBack)
+        gamepad.onAxis(scrollElements)
+        //bind pega o contexto (this) para poder utilizar as funções definidas nesse escopo
+    }
+    requestAnimationFrame(this.gamepadLoop);
+}
+
 function changeStmPath() {
     document.querySelector('.effectiveArea').innerHTML = ''
 }
@@ -14,6 +30,12 @@ const actions = {
 }
 
 const configs = document.querySelectorAll('.config')
-configs.forEach((element) => element.addEventListener('click', (ev) => {
+configs.forEach((element, idx) => {
+    element.tabIndex = idx
+    element.addEventListener('click', (ev) => {
     actions[element.id].call(this)
-}))
+    })
+})
+
+configs[0].focus()
+requestAnimationFrame(gamepadLoop)
