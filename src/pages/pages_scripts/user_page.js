24fc568b8e8663736261be125a2bdb6f2b08b663
Vinícius Lameirão <viniciuslameirao@hotmail.com>
2025-01-03 20:08:52 -0300

    add: user page

---
+++ src/pages/pages_scripts/user_page.js
@@ -0,0 +1,8 @@
+// TODO Integrates the user page with the backend
+// TODO Save cookies
+// TODO Bring sent/solicitations/recieved friends
+// TODO Bring Users online
+// TODO Users profile settings
+// TODO Users privacy settings
+// TODO Bring user's profile
+// ? Maybe bring the login system here
