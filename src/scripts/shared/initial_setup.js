0e11cb320289343520abef87a7157ef8ccf2b1c0
mathyc0de <mateushackrs7@gmail.com>
2025-02-17 21:38:23 -0300

    ✨ feat: find steam path from windows registry

--- src/scripts/shared/initial_setup.js
+++ src/scripts/shared/initial_setup.js
@@ -1,46 +1,77 @@
-const { mkdirSync, writeFileSync, readFileSync } = require("fs");
+const { mkdirSync, writeFileSync, readFileSync, existsSync } = require("fs");
+const {execSync} = require('child_process')
 const path = require("path");
-const VDF = require('vdf-parser');
 
 
-function getSteamLibs(env) {
-    const text = readFileSync(path.join(env['ProgramFiles(x86)'], '/Steam/steamapps/', 'libraryfolders.vdf'))
-    const parsed = VDF.parse(text.toString('utf-8'))
-    const libraryfolders = parsed.libraryfolders
-    const libs = []
-    for (let lib in libraryfolders) {
-        libs.push(path.normalize(libraryfolders[lib].path))
+// const VDF = require('vdf-parser');
+
+
+// function getSteamLibs(steam_path) {
+//     const text = readFileSync(path.join(env['ProgramFiles(x86)'], '/Steam/steamapps/', 'libraryfolders.vdf'))
+//     const parsed = VDF.parse(text.toString('utf-8'))
+//     const libraryfolders = parsed.libraryfolders
+//     const libs = []
+//     for (let lib in libraryfolders) {
+//         libs.push(path.normalize(libraryfolders[lib].path))
+//     }
+//     return libs
+// }
+
+
+function getLibs(obj) {
+    try {
+            const steam_path = execSync('reg query "HKEY_CURRENT_USER\\Software\\Valve\\Steam" /v SteamPath')
+                    .toString()
+                    .split('REG_SZ')[1]
+                    .trim();
+                obj['steam'] = steam_path;
+            } catch (error) {
+                console.error("Error finding Steam path in registry:", error);
+            }
+    if (existsSync('C:\\ProgramData\\Epic\\EpicGamesLauncher\\Data\\Manifests')) {
+        obj['epic'] = 'C:/ProgramData/Epic/EpicGamesLauncher/Data/Manifests/'
     }
-    return libs
 }
 
-function linuxSetup(env) {
+
+function linuxSetup() {
     // mkdirSync("")
 }
 
-function windowsSetup(env) {
-    getSteamLibs(env)
-    const ghub_path = path.join(env.LOCALAPPDATA, '/gaming_hub/')
+function windowsSetup() {
+    const ghub_path = path.join(process.env.LOCALAPPDATA, '/gaming_hub/')
+    const lib = {}
+    getLibs(lib)
     mkdirSync(path.join(ghub_path, 'assets/images'), {recursive: true})
-    writeFileSync(ghub_path + 'config.json', JSON.stringify({
-        steam: {
-            libraries: getSteamLibs(env)
-        }
-    }))
+    writeFileSync(ghub_path + 'config.json', JSON.stringify(lib))
     writeFileSync(ghub_path + 'steamapps.json', '[]')
+    writeFileSync(ghub_path + 'epicapps.json', '[]')
     writeFileSync('.env', `APP_PATH=${ghub_path}`)
     writeFileSync(path.join(ghub_path, 'game_data.json'), '{}')
 }
 
-function setupPath(platform, env) {
+function setupPath(platform) {
     switch (platform) {
         case 'linux':
-            linuxSetup(env)
+            linuxSetup()
             break
         case 'win32':
-            windowsSetup(env)
+            windowsSetup()
             break;
     }
 }
 
-module.exports = {setupPath, getSteamLibs}
+function refreshLibs() {
+    const localLibs = JSON.parse(readFileSync(process.env.APP_PATH + 'config.json'))
+    const libs = ['steam', 'epic', 'uplay', 'ea']
+    libs.forEach((lib) => {
+        if (!localLibs[`${lib}`]) {
+            getLibs(lib, localLibs)
+        }
+    })
+    writeFileSync(process.env.APP_PATH + 'config.json', JSON.stringify(localLibs))
+
+}
+
+
+module.exports = {setupPath}
