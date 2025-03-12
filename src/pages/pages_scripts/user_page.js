1606a464a7773514a175e3edc944d78469b30bbb
Vinícius <viniciuslameirao@hotmail.com>
2025-03-12 15:23:54 -0300

    Redirect to user managemenet page

--- src/pages/pages_scripts/user_page.js
+++ src/pages/pages_scripts/user_page.js
@@ -6,3 +6,9 @@
 // TODO Users privacy settings
 // TODO Bring user's profile
 // ? Maybe bring the login system here
+
+const userManagementPage = document.querySelector('.user-management-page')
+
+userManagementPage.addEventListener('click', () => {
+    window.open("http://localhost:5244/index.html", "User Management", "width=800,height=800")
+})
